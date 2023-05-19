7583fe38cfc29ec73e4a9ee1cd49987dfa086025
Osayi Akoko <osayistreams@gmail.com>
2023-05-19 11:29:07 +0100

    populate genre with CommandRunner

--- src/main/java/com/osayistreams/moviereview/MovieReviewApplication.java
+++ src/main/java/com/osayistreams/moviereview/MovieReviewApplication.java
@@ -1,7 +1,12 @@
 package com.osayistreams.moviereview;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.osayistreams.moviereview.entities.Genre;
+import com.osayistreams.moviereview.services.genre.GenreService;
 
 @SpringBootApplication
 public class MovieReviewApplication {
@@ -10,4 +15,21 @@ public class MovieReviewApplication {
 		SpringApplication.run(MovieReviewApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner populateGenre(GenreService service){
+		return args -> {
+			if (service.findAllGenre().isEmpty()){
+				service.save(Genre.builder().name("Action").build());
+				service.save(Genre.builder().name("Comendy").build());
+				service.save(Genre.builder().name("Science fiction").build());
+				service.save(Genre.builder().name("Adventure").build());
+				service.save(Genre.builder().name("History").build());
+				service.save(Genre.builder().name("Horror").build());
+				service.save(Genre.builder().name("Epic").build());
+				service.save(Genre.builder().name("Mystery").build());
+				service.save(Genre.builder().name("Romance").build());
+				service.save(Genre.builder().name("Drama").build());
+			}
+		};
+	}
 }
