1155941c43056d9f8974e66432a7d6640c9c2dc0
Osayi Akoko <osayistreams@gmail.com>
2023-05-19 10:27:17 +0100

    add movie & genre relationship

--- src/main/java/com/osayistreams/moviereview/entities/Genre.java
+++ src/main/java/com/osayistreams/moviereview/entities/Genre.java
@@ -1,8 +1,11 @@
 package com.osayistreams.moviereview.entities;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,4 +20,7 @@ public class Genre {
     private Long id;
 
     private String name;
+
+    @ManyToMany(mappedBy = "genres")
+    private List<Movie> movies;
 }
--- src/main/java/com/osayistreams/moviereview/entities/Movie.java
+++ src/main/java/com/osayistreams/moviereview/entities/Movie.java
@@ -1,10 +1,17 @@
 package com.osayistreams.moviereview.entities;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,17 +24,33 @@ public class Movie {
 
     @Id
     @GeneratedValue
-    private Integer id;
+    private Long id;
 
+    @Column(nullable = false)
     private String title;
 
+    @Lob
     private String overview;
 
     private String posterUrl;
 
+    @ManyToMany(cascade = CascadeType.REMOVE)
+    @JoinTable(
+        name = "movie_genre",
+        joinColumns = @JoinColumn(name = "movie_id"),
+        inverseJoinColumns = @JoinColumn(name = "genre_id")
+    )
+    private List<Genre> genres;
+
     private LocalDate releaseDate;
 
-    private int voteCount;
+    @Column(nullable = false)
+    private Integer ratingCount;
     
-    private float voteAverage;
+    @Column(nullable = false)
+    private Float ratingAverage;
+
+    private LocalDate createdAt;
+
+    private LocalDate updatedAt;
 }
