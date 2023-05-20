094d1c208a8fbc8de8b9a33d46d402756fc35b8e
Osayi Akoko <osayistreams@gmail.com>
2023-05-20 16:13:05 +0100

    remove ManyToMany mapping

--- src/main/java/com/osayistreams/moviereview/entities/Genre.java
+++ src/main/java/com/osayistreams/moviereview/entities/Genre.java
@@ -1,12 +1,8 @@
 package com.osayistreams.moviereview.entities;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,7 +19,5 @@ public class Genre {
     private Integer id;
 
     private String name;
-
-    @ManyToMany(mappedBy = "genres")
-    private Set<Movie> movies = new HashSet<>();
 }
+ 
