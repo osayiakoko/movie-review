58e2bd9a34e2e0b390510518af36365dc373ad2a
Osayi Akoko <osayistreams@gmail.com>
2023-05-21 02:22:32 +0100

    add movie repository

---
+++ src/main/java/com/osayistreams/moviereview/repositories/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.osayistreams.moviereview.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.osayistreams.moviereview.entities.Movie;
+
+
+public interface MovieRepository extends JpaRepository<Movie, Integer>{
+    Movie findByTitle(String title);
+}
